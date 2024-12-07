13e37f6ab8c1115ab0308734111732f00844cd16
juliana <juliana.li@csedge.org>
2024-12-07 10:41:01 -0800

    blah

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
+from tinydb import TinyDB, Query
 
 app = Flask(__name__)
+db=TinyDB('db.json')
 
 @app.route('/', methods=['GET', 'POST'])
 def todo():
     todo = request.form.get('todo')
-    return render_template('index.html')
+    if todo:
+        db.insert({'todo':todo})
+    todos= db.all()
+    return render_template('index.html', todos=todos)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
